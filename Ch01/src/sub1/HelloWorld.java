dfba40ec6a1468357d1831e5baddc99888e3fbb3
LK920 <hopeone1214@gmail.com>
2020-05-27 12:30:30 +0900

    ch01 수정

--- Ch01/src/sub1/HelloWorld.java
+++ Ch01/src/sub1/HelloWorld.java
@@ -9,12 +9,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 	System.out.println("HelloWorld!");
 	System.out.println("Hello Java!");
-	System.out.println("Hello Java!");
-	System.out.println("Hello Java!");
-	System.out.println("Hello Java!");
-	System.out.println("Hello Java!");
-	System.out.println("HelloWorld~~");
-	System.out.println("Hello JAVA~~~");
+	
 	}
 	
 }
